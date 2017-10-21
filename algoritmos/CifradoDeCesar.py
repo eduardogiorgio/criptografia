2de6ef58492dcde0ed2cf1636289f6b330dc124e
sergio <sergio.dp@hotmail.com>
2017-10-21 16:47:35 -0300

    cesar

--- algoritmos/CifradoDeCesar.py
+++
@@ -1,3 +0,0 @@
-def CifrarSinCesar(msj,dsp):                                                                    #msj= mensaje a cifrar; dsp= desplazamiento (entero+-)
-    abc='abcdefghijklmnñopqrstuvwxyz '                                                          #alfabeto
-    return ''.join([(abc[abc.find(msj.lower()[i])-(dsp%len(abc))]) for i in range(len(msj))])
